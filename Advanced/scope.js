3f5305d7348a049dc65c8c9badee0962a3d7b55a
heinwithsmile <heinhtetaung7715@gmail.com>
2024-07-01 23:45:22 +0630

    third

---
+++ Advanced/scope.js
@@ -0,0 +1,74 @@
+// 'window' is globally scoped
+// alert('Hello');
+// console.log(innerWidth);
+
+// Global scope variable
+const x = 100;
+
+console.log(x, 'in global');
+
+function run() {
+  // Access global vars in functions
+  console.log(window.innerHeight);
+  console.log(x, 'in function');
+}
+
+run();
+
+// Access global vars in blocks
+if (true) {
+  console.log(x, 'in block');
+}
+
+function add() {
+  // Overwriting global x (variable shadowing)
+  const x = 1;
+  const y = 50;
+  console.log(x + y);
+}
+
+// Can not access a function scoped variable in global scope
+console.log(y);
+
+add();
+
+const x = 100;
+
+// An If statement is a block
+if (true) {
+  console.log(x);
+  const y = 200;
+  console.log(x + y);
+}
+
+// console.log(y); // ReferenceError: y is not defined
+
+// A loop is a block
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// console.log(i); // ReferenceError: i is not defined
+
+// Using var
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+console.log(c);
+
+// var IS function scoped
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+run();
+
+// console.log(d);
+
+
+const foo = 1;
+var bar = 2; // Put on the window object
